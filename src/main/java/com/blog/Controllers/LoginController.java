306b026ef661cf54049e37a5a815ee9b19bc57c8
Jarrison Cano <jarrison.cano@hotmail.com>
2022-11-19 01:12:53 -0500

    Implement login feature

--- src/main/java/com/blog/Controllers/LoginController.java
+++ src/main/java/com/blog/Controllers/LoginController.java
@@ -1,42 +1,86 @@
 package com.blog.Controllers;
 
 import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import com.blog.Models.User;
+import com.blog.Repositories.UserRepository;
+
 
 @Controller
 public class LoginController {
+
+	
+	@Autowired
+    UserRepository userRepository;
+	
+	private final InMemoryUserDetailsManager inMemoryUserDetailsManager;
+
+    @Autowired
+    public LoginController(InMemoryUserDetailsManager inMemoryUserDetailsManager) {
+       this.inMemoryUserDetailsManager = inMemoryUserDetailsManager;
+    }
+
        
     @GetMapping("/login")
 	public String Index(Model model) {
 		try {
-			// List<Blog> blogs = blogRepository.findAll();
-
-			// model.addAttribute("blogs", blogs);
 			return "login";
 
 		}catch (Exception e){
+			System.out.println(e.toString());
 			return "error";
 		}
 	}
 
-    // @PostMapping("/login")
-	// public String login(@ModelAttribute Blog blog, Model model) {
-	// 	try {
+    @GetMapping("/register")
+	public String Register(Model model) {
+		try {
+			return "register";
+
+		}catch (Exception e){
+			System.out.println(e.toString());
+			return "error";
+		}
+	}
+
+    @PostMapping("/register")
+	public String RegisterUser(@ModelAttribute User user, Model model) {
+		try {
+			if(user.getName() == "" || user.getUsername() == "" || user.getUsername().contains(" ") || user.getPassword() == "" ){
+				model.addAttribute("error", "Algunos datos son incorrectos");
+				return Register(model);
+
+			}
+			if(userRepository.findByUsername(user.getUsername()) != null){
+				model.addAttribute("error", "El usuario ya existe");
+				return Register(model);
+			}
 			
-	// 		if(blog.getTitle() =="" || blog.getContent() == "") {
-	// 			model.addAttribute("error", "Title or Content is empty");
-	// 			return "newBlog";
-	// 		}
-	// 		blogRepository.save(new Blog(blog.getTitle(), blog.getContent(), true));
-	// 		return Index(model);
+			userRepository.save(new User(user.getName(), user.getUsername().toLowerCase(), user.getPassword()));
+			inMemoryUserDetailsManager.createUser(org.springframework.security.core.userdetails.User.withDefaultPasswordEncoder().username(user.getUsername().toLowerCase()).password(user.getPassword()).roles("USER").build());
+			return Index(model);
 		
 
-	// 	} catch (Exception e) {
-	// 		return "error";
-	// 	}
-	// }
+		}catch (Exception e){
+			System.out.println(e.toString());
+			return "error";
+		}
+	}
+
+	@GetMapping("/logout")
+	public String Logout(Model model) {
+		try {
+			return "redirect:/login";
+
+		}catch (Exception e){
+			System.out.println(e.toString());
+			return "error";
+		}
+	}
 
 }
--- src/main/java/com/blog/securingWeb/WebSecurityConfig.java
+++ src/main/java/com/blog/securingWeb/WebSecurityConfig.java
@@ -1,24 +1,88 @@
 package com.blog.securingWeb;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import java.util.List;
+import java.util.Properties;
+
+import com.blog.Repositories.UserRepository;
+
+@EnableAutoConfiguration(exclude = SecurityAutoConfiguration.class)
+
+// @Configuration
+// @EnableWebSecurity
+// public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+// 	@Autowired
+//     UserRepository userRepository;
+
+//     // @Bean
+// 	// public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+// 	// 	http
+// 	// 		.authorizeHttpRequests((requests) -> requests
+// 	// 			.antMatchers("/login", "/register").permitAll()
+// 	// 			.anyRequest().authenticated()
+// 	// 		)
+// 	// 		.formLogin((form) -> form
+// 	// 			.loginPage("/login")
+// 	// 			.permitAll()
+// 	// 		)
+// 	// 		.logout((logout) -> logout.permitAll());
+
+// 	// 	return http.build();
+// 	// }
+
+// 	// @Bean
+// 	// public UserDetailsService userDetailsService() {
+// 	// 	List<com.blog.Models.User> users = userRepository.findAll();
+
+// 	// 	UserDetails[] userDetails = new UserDetails[users.size()];
+
+// 	// 	for (com.blog.Models.User user : users) {
+// 	// 		userDetails[users.indexOf(user)] = User.withDefaultPasswordEncoder().username(user.getUsername()).password(user.getPassword()).roles("USER").build();
+// 	// 	}
+
+// 	// 	return new InMemoryUserDetailsManager(userDetails);
+// 	// }
+
+// 	@Override
+//     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//         auth.userDetailsService(inMemoryUserDetailsManager());
+//     }
+
+// 	@Bean
+//     public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+//         final Properties users = new Properties();
+//         users.put("user","pass,ROLE_USER,enabled"); //add whatever other user you need
+//         return new InMemoryUserDetailsManager(users);
+//     }
+    
+// }
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+    UserRepository userRepository;
 
-    @Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       		http
 			.authorizeHttpRequests((requests) -> requests
-				.antMatchers().permitAll()
+				.antMatchers("/login", "/register").permitAll()
 				.anyRequest().authenticated()
 			)
 			.formLogin((form) -> form
@@ -26,20 +90,25 @@ public class WebSecurityConfig {
 				.permitAll()
 			)
 			.logout((logout) -> logout.permitAll());
+    }
 
-		return http.build();
-	}
 
-	@Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(inMemoryUserDetailsManager());
+    }
 
-		return new InMemoryUserDetailsManager(user);
-	}
-    
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+		List<com.blog.Models.User> users = userRepository.findAll();
+
+		UserDetails[] userDetails = new UserDetails[users.size()];
+
+		for (com.blog.Models.User user : users) {
+			userDetails[users.indexOf(user)] = User.withDefaultPasswordEncoder().username(user.getUsername()).password(user.getPassword()).roles("USER").build();
+		}
+
+		return new InMemoryUserDetailsManager(userDetails);
+    }
+
 }
